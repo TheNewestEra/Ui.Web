41160241c54cf3c104746337916e7874d3ecf5d0
Raaga Krishna <vraagakrishna@gmail.com>
2026-08-06 17:06:54 +0200

    Added Card Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { ButtonComponent } from "@shared/components/button/button";
+import { CardComponent } from "@shared/components/card/card";
 
 @Component({
   selector: "app-root",
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CardComponent],
   templateUrl: "./app.html",
   styleUrl: "./app.css",
 })
