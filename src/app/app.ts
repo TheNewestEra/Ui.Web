50ffcce3695deae11e5e4bdb19ecec9d33cc361c
Raaga Krishna <vraagakrishna@gmail.com>
2026-08-06 16:44:18 +0200

    Fixed tailwind setup + Added prettier

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: "app-root",
+  imports: [],
+  templateUrl: "./app.html",
+  styleUrl: "./app.css",
 })
-export class App {
-  protected readonly title = signal('angular-starter');
-}
+export class App {}
